80d45bc56ba6198be20f7e11c7312f9b1bbba851
Bey Sviatoslav Igorevich <bey.sviatoslav@yandex.ru>
2020-07-05 02:17:30 +0300

    creating of group

---
+++ lib/Catalog/variants/entities/Group.dart
@@ -0,0 +1,10 @@
+
+class Group{
+  var _groupId;
+  var _title;
+
+  get groupId => _groupId;
+  get title => _title;
+
+  Group(this._groupId, this._title);
+}
--- lib/Catalog/variants/view/VariantsView.dart
+++ lib/Catalog/variants/view/VariantsView.dart
@@ -1,15 +1,21 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp/Catalog/variants/entities/Group.dart';
 import 'package:flutterapp/Catalog/variants/presenter/VariantsPresenter.dart';
 import 'package:flutterapp/Catalog/variants/view/Item.dart';
 
 class VariantsView extends StatelessWidget{
 
   var _variantsPresenter;
+  List<Group> groups;
 
   VariantsView(VariantsPresenter _variantsPresenter){
     this._variantsPresenter = _variantsPresenter;
+    groups = List<Group>();
+    groups.add(Group("123", "M32031"));
+    groups.add(Group("123", "M32031"));
+    groups.add(Group("123", "M32031"));
   }
 
   @override
@@ -22,26 +28,33 @@ class VariantsView extends StatelessWidget{
   Widget buildGridView(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 5),
-      child: GridView.count(
-        crossAxisCount: 3,
-        children: <Widget>[
-          new Item(0.75, "M32031", Color.fromRGBO(239, 87, 83, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "M3102", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "K2308", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.75, "M32031", Color.fromRGBO(239, 87, 83, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "M3102", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "K2308", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.75, "M32031", Color.fromRGBO(239, 87, 83, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "M3102", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "K2308", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.75, "M32031", Color.fromRGBO(239, 87, 83, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "M3102", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "K2308", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.75, "M32031", Color.fromRGBO(239, 87, 83, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "M3102", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-          new Item(0.34, "K2308", Color.fromRGBO(131, 34, 167, 1), _variantsPresenter.catalogPresenter),
-        ],
-      ),
+      child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemCount: groups.length,
+          itemBuilder: (_, index){
+            return Center(
+                child: GestureDetector(
+                    onTap: () {
+                      _variantsPresenter.catalogPresenter.goToDayTimetable(context);
+                    },
+                    child: Container(
+                        height: 100,
+                        width: 100,
+                        child: Card(
+                            color:  _variantsPresenter.catalogPresenter.mainPresenter.mainPresenterModel.themeColorEnd,
+                            elevation: 20,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: Center(child: Text(
+                              "${groups[index].title}",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ))))));
+          }),
     );
   }
 }
